b9d91675778f9cb89bdc4a643a50d81ff1043886
Amr Lotfy <amr.lotfy.othman@gmail.com>
2025-11-01 21:30:03 +0200

    Update Firebase hosting configuration to use matrix-delivery project
    - Update Firebase config with new project credentials
    - Change default Firebase project to matrix-delivery
    - Update CORS_ORIGIN to include new Firebase hosting URL
    - Update GitHub Actions workflows for new project
    - Update documentation and example files

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDinn2c3TmEEtVOl0s3jRhFSzdmFOjPncs",
-  authDomain: "badr-delivery-dev.firebaseapp.com",
-  projectId: "badr-delivery-dev",
-  storageBucket: "badr-delivery-dev.firebasestorage.app",
-  messagingSenderId: "563643637401",
-  appId: "1:563643637401:web:bd05d7e7ff4558eedc2208",
-  measurementId: "G-HTMH7MM0MT"
+  apiKey: "AIzaSyCKLqK_x_Jvop7a5ht3w1nsnpa2hhx1bVk",
+  authDomain: "matrix-delivery.firebaseapp.com",
+  projectId: "matrix-delivery",
+  storageBucket: "matrix-delivery.firebasestorage.app",
+  messagingSenderId: "127557882021",
+  appId: "1:127557882021:web:f515e53b8d66547d2efd4c",
+  measurementId: "G-WE5CMMR9LB"
 };
 
 // Initialize Firebase
