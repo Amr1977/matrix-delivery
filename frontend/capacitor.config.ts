d9c2680fc2076bad31289564f3eaed3dd39be9c0
Amr Lotfy <amr.lotfy.othman@gmail.com>
2026-03-08 00:19:00 +0200

    wip: create android app

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.badr.delivery',
-  appName: 'BadrDeliveryTest',
+  appId: 'com.matrix.delivery',
+  appName: 'Matrix Delivery',
   webDir: 'build'
 };
 
